816e6332a41e8e291463b065ea42bf2e51db642d
@alessandro-cav <alessandrocav14@gmail.com>
2023-03-11 12:58:23 -0300

    Configurando a api gateway cloud

--- ms-api-gateway-cloud/src/main/java/br/com/ms/api/gateway/cloud/MsApiGatewayCloudApplication.java
+++ ms-api-gateway-cloud/src/main/java/br/com/ms/api/gateway/cloud/MsApiGatewayCloudApplication.java
@@ -2,7 +2,9 @@ package br.com.ms.api.gateway.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsApiGatewayCloudApplication {
 
