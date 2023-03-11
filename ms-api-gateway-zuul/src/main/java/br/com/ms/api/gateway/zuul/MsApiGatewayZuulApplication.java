24709c8e1c0e06dfe8526bf37942187c85821b38
@alessandro-cav <alessandrocav14@gmail.com>
2023-03-11 09:18:42 -0300

    Criando a api gateway zull para ser a porta de entrada para requisiçoes externas

---
+++ ms-api-gateway-zuul/src/main/java/br/com/ms/api/gateway/zuul/MsApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.api.gateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ ms-api-gateway-zuul/src/test/java/br/com/ms/api/gateway/zuul/MsApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms.api.gateway.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
