4e11f328a7738963096664b20755f06c8e5eed22
@alessandro-cav <alessandrocav14@gmail.com>
2023-03-11 10:59:54 -0300

    Criando o api gateway cloud

---
+++ ms-api-gateway-cloud/src/main/java/br/com/ms/api/gateway/cloud/MsApiGatewayCloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.api.gateway.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiGatewayCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayCloudApplication.class, args);
+	}
+
+}
---
+++ ms-api-gateway-cloud/src/test/java/br/com/ms/api/gateway/cloud/MsApiGatewayCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms.api.gateway.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
